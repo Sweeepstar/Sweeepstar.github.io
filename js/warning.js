ea41911d407f0c6958125b682c65f8653b134e1a
Sweeepstar <Qwerstar3@gmail.com>
2016-11-16 23:46:29 +0100

    Warning js fix?

--- js/warning.js
+++ js/warning.js
@@ -1,28 +1,8 @@
-Object.defineProperty(window, "console", {
-    value: console,
-    writable: false,
-    configurable: false
-});
-
-var i = 0;
-function showWarningAndThrow() {
-    if (!i) {
-        setTimeout(function () {
-            console.log("%cWarning message", "font: 2em sans-serif; color: yellow; background-color: red;");
-        }, 1);
-        i = 1;
+window.console.log = function(){
+    console.error('The developer console is temp...');
+    window.console.log = function() {
+        return false;
     }
-    throw "Console is disabled";
 }
 
-var l, n = {
-        set: function (o) {
-            l = o;
-        },
-        get: function () {
-            showWarningAndThrow();
-            return l;
-        }
-    };
-Object.defineProperty(console, "_commandLineAPI", n);
-Object.defineProperty(console, "__commandLineAPI", n);
+console.log('test');
