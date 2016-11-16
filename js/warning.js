a42c11b9d08c553c00f9a4084e06a9f16d28a17f
Sweeepstar <Qwerstar3@gmail.com>
2016-11-16 23:47:46 +0100

    Warning js stylelöondsaföinsahafnsdpa

--- js/warning.js
+++ js/warning.js
@@ -1,5 +1,5 @@
 window.console.log = function(){
-    console.error('The developer console is temp...');
+    console.error('%c Oh my heavens! ', 'background: #222; color: #bada55');
     window.console.log = function() {
         return false;
     }
